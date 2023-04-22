b1bd586b12472e584c2177ecabe91d868cd51d53
曹珂俭 <1849201815@qq.com>
2023-04-22 19:16:00 +0800

    抽奖bug

--- subpkg/toVideoDetail/toVideoDetail.vue
+++ subpkg/toVideoDetail/toVideoDetail.vue
@@ -16,21 +16,24 @@
 			<my-issus :isshowMseeage='isshowMseeage' :isShowSu="isShowSu"></my-issus>
 		</view>
 		<view class="selectContent" v-show="videoItem.length" v-if="isshowSelect">
-			<span v-if="showSelected">恭喜!抽中了<span style="color: #CE5353;margin:0 18rpx;font-size:22px;font-weight: bold;">{{selected}}</span>!</span>
+			<span v-if="showSelected">恭喜!抽中了<span
+					style="color: #CE5353;margin:0 18rpx;font-size:22px;font-weight: bold;">{{selected}}</span>!</span>
 			<span v-if="showIng">正在抽奖...</span>
 			<span v-if="showStart">点开始按钮抽奖</span>
 		</view>
 		<view class="ul" v-show="videoItem.length" v-if="isshowSelect">
-			<li :class="index<4?`li${index+1}`:
+			<li :class="index<3?`li${index+1}`:
+									index==3?`li8`:
 									index==4?`start`:
-									`li${index}`" 
-				v-for="item,index in selectContent" :key="item" 
-				@click="handleClick(index)"
->{{selectContent[index]}}</li>
+									index==5?`li4`:
+									index>5?`li${13-index}`:
+									`li${index}`" v-for="item,index in selectContent" :key="item" @click="handleClick(index)">
+				{{selectContent[index]}}</li>
 		</view>
-			<uni-popup ref="inputDialog" type="dialog">
-				<uni-popup-dialog mode="input" placeholder="填写你想选择的事件名!" message="成功消息" :duration="2000" :before-close="true" @close="close" @confirm="confirm"></uni-popup-dialog>
-			</uni-popup>
+		<uni-popup ref="inputDialog" type="dialog">
+			<uni-popup-dialog mode="input" placeholder="填写你想选择的事件名!" message="成功消息" :duration="2000"
+				:before-close="true" @close="close" @confirm="confirm"></uni-popup-dialog>
+		</uni-popup>
 	</view>
 </template>
 
@@ -45,24 +48,24 @@
 				videoItem: '',
 				isshowMseeage: true,
 				isShowSu: false,
-				isshowInput:false,
-				isshowSelect:false,
+				isshowInput: false,
+				isshowSelect: false,
 				// 抽奖
-				disabled:false,
-				selectContent:['螺蛳粉','臭豆腐','花甲粉','烤鱼饭','开始','上汤上面','鸡扒饭','重庆小面','火锅'],
-				content:'',
-				selected:'',
-				showSelected:false,
-				showIng:false,
-				showStart:true,
-				rollTime:null,
-				times:0,
-				time:0,
-				index:undefined
+				disabled: false,
+				selectContent: ['螺蛳粉', '臭豆腐', '花甲粉', '烤鱼饭', '开始', '上汤上面', '鸡扒饭', '重庆小面', '火锅'],
+				content: '',
+				selected: '',
+				showSelected: false,
+				showIng: false,
+				showStart: true,
+				rollTime: null,
+				times: 0,
+				time: 0,
+				index: undefined
 			};
 		},
 		methods: {
-			dialogInputConfirm(){
+			dialogInputConfirm() {
 				this.$refs.inputDialog.open()
 			},
 			// 加载时触发
@@ -77,89 +80,88 @@
 				})
 			},
 			readUpdata() {
-				utilsObj.operation('videoClass', "view_coun", this.videoId, 2).then(res => {
-				})
+				utilsObj.operation('videoClass', "view_coun", this.videoId, 2).then(res => {})
 			},
 			// 抽奖
-			handleClick(index){
-				if(this.disabled) return
+			handleClick(index) {
+				if (this.disabled) return
 				this.index = index
-				this.content =''
-				if(index==4){
-					if(!this.disabled){
+				this.content = ''
+				if (index == 4) {
+					if (!this.disabled) {
 						this.start()
 					}
-				}else{
+				} else {
 					this.dialogInputConfirm()
 				}
 			},
-			confirm(e){
+			confirm(e) {
 				this.content = e
-				if(e!==''){
-					this.selectContent.splice(this.index,1,this.content)
+				if (e !== '') {
+					this.selectContent.splice(this.index, 1, this.content)
 				}
 				this.$refs.inputDialog.close()
 			},
-			close(){
+			close() {
 				this.$refs.inputDialog.close()
 			},
-			start(){
+			start() {
 				this.disabled = true
 				this.selected = ''
-				if(this.index!==undefined){
+				if (this.index !== undefined) {
 					document.querySelector(`.li${this.index}`).classList.remove("active")
 				}
-				this.showSelected=false,
-				this.showIng = true
+				this.showSelected = false,
+					this.showIng = true
 				this.showStart = false
 				this.times = parseInt(Math.random() * (20 - 30 + 1) + 20, 10)
 				this.rolling();
 			},
-			rolling(){
+			rolling() {
 				this.time++
 				clearTimeout(this.rollTime);
-				  this.rollTime = setTimeout(() => {
+				this.rollTime = setTimeout(() => {
 					window.requestAnimationFrame(this.rolling); // 进行递归动画
 				}, 250);
-				  if(this.time>8){
-				    this.index=this.time%8;
-				    if(this.index==0){
-				      this.index=8
-				    }
-				  }else{
-				    this.index=this.time
-				  }
-					document.querySelector(`.li${this.index}`).classList.add("active")
-					if(this.index>1){
-						document.querySelector(`.li${this.index - 1}`).classList.remove("active")
-					}else if(this.index == 1){
-						document.querySelector(`.li8`).classList.remove("active")
+				if (this.time > 8) {
+					this.index = this.time % 8;
+					if (this.index == 0) {
+				 	this.index = 8
 					}
-				  // time > times 转动停止
-				  if (this.time > this.times) {
-				    clearInterval(this.rollTime);
-						 this.disabled = false
-						this.showIng = false
-						this.showSelected = true
-				    this.time = 0;
-						this.selected =  document.querySelector(`.li${this.index}`).textContent
-				    return;
-				  }
+				} else {
+					this.index = this.time
+				}
+				document.querySelector(`.li${this.index}`).classList.add("active")
+				if (this.index > 1) {
+					document.querySelector(`.li${this.index - 1}`).classList.remove("active")
+				} else if (this.index == 1) {
+					document.querySelector(`.li8`).classList.remove("active")
+				}
+				// time > times 转动停止
+				if (this.time > this.times) {
+					clearInterval(this.rollTime);
+					this.disabled = false
+					this.showIng = false
+					this.showSelected = true
+					this.time = 0;
+					this.selected = document.querySelector(`.li${this.index}`).textContent
+					return;
+				}
 			}
 		},
 		mounted() {
-			const a =document.querySelector('.start').style.background='#f3d163'
+			const a = document.querySelector('.start').style.background = '#f3d163'
 		},
 		onLoad(e) {
 			this.videoId = e.id
-				this.readUpdata()
+			this.readUpdata()
 			this.getData()
-			if(!this.videoItem){
+			if (!this.videoItem) {
 				this.isshowInput = false
 			}
 			if (e.even === '1') {
 				this.isshowInput = true
-			}else if(e.even === '0'){
+			} else if (e.even === '0') {
 				this.isshowSelect = true
 			}
 		}
@@ -171,16 +173,18 @@
 		// 组件
 		background-color: #fffae5;
 	}
+
 	.uni-section .uni-section-header__decoration[data-v-f7ca1098] {
-	    margin-right: 6px;
-	    background-color: #f4ffbe;
+		margin-right: 6px;
+		background-color: #f4ffbe;
 	}
+
 	.wrapper {
 		width: 100%;
 		height: 100vh;
 		background-color: #fffae5;
-	
-	.item-f {
+
+		.item-f {
 			width: 100%;
 
 			.video {
@@ -194,48 +198,53 @@
 			}
 		}
 	}
+
 	// 抽奖
-	.select{
+	.select {
 		width: 90%;
 		height: 600rpx;
 		margin: 0 auto;
 		background-color: #fff;
 		box-sizing: border-box;
 	}
+
 	.ul {
 		margin-top: 20rpx;
-	  display: flex;
-	  flex-wrap: wrap;
-	  justify-content: space-between;
+		display: flex;
+		flex-wrap: wrap;
+		justify-content: space-between;
 		width: 100%;
 		height: auto;
 		align-items: center;
 	}
+
 	li {
 		width: 25%;
 		height: 25%;
 	}
-	
+
 	.ul li {
-	  box-sizing: border-box;
-	  margin: 10px;
-	  list-style: none;
-	  line-height: 100px;
-	  text-align: center;
-	  font-size: 18px;
-	  border-radius: 10px;
-	  cursor: pointer;
+		box-sizing: border-box;
+		margin: 10px;
+		list-style: none;
+		line-height: 100px;
+		text-align: center;
+		font-size: 18px;
+		border-radius: 10px;
+		cursor: pointer;
 		background: #ffeb99;
-	  // background: linear-gradient(90deg, #72a8f8 0%, #3e93d3 50%, #5385e2 100%);
-	  // text-shadow: 5px 5px 5px #1004ad;
-	  color: #9E6D38;
+		// background: linear-gradient(90deg, #72a8f8 0%, #3e93d3 50%, #5385e2 100%);
+		// text-shadow: 5px 5px 5px #1004ad;
+		color: #9E6D38;
 		font-weight: bold
 	}
+
 	.ul li.active {
-	  background: #deefff;
-	  color: #9E6D38;
+		background: #deefff;
+		color: #9E6D38;
 	}
-	.selectContent{
+
+	.selectContent {
 		width: 95%;
 		height: 80rpx;
 		background-color: #edc00c;
@@ -246,7 +255,4 @@
 		color: #9E6D38;
 		font-size: 18px;
 	}
-
 </style>
-
-
